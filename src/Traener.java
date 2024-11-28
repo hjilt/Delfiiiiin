788882a5e45ca627910be32639be2e0aa27fc3a4
israfil005 <isha0001@stud.kea.dk>
2024-11-28 13:01:34 +0100

    hej

---
+++ src/Traener.java
@@ -0,0 +1,41 @@
+public class Traener {
+    String navn;
+    String hold;
+    String skema;
+
+    public Traener(String navn, String hold, String skema){
+        this.navn = navn;
+        this.hold = hold;
+        this.skema = skema;
+
+    }
+    public String getNavn(){
+        return navn;
+    }
+
+    public void setNavn(String navn){
+        this.navn = navn;
+    }
+
+    public String getHold(){
+        return hold;
+    }
+
+    public void setHold(String hold){
+        this.hold = hold;
+    }
+
+    public String getSkema() {
+        return skema;
+    }
+
+    public void setSkema(String skema){
+        this.skema = skema;
+    }
+
+
+
+
+}
+
+
