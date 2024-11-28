9606a213dc950b962e54ce14967ab1b22d8211d6
Tarik2300 <tana0001@stud.kea.dk>
2024-11-28 12:54:32 +0100

    Member og test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hello test!");
+=======
+
+        Medlem medlem1 = new Medlem( "Tarik Naji", "Mand", LocalDate.of(1996, 1, 8), true);
+
+        System.out.println("Medlem 1:");
+        System.out.println("Fulde navn: " + medlem1.getFuldeNavn());
+        System.out.println("Køn: " + medlem1.getKoen());
+        System.out.println("Fødselsdato: " + medlem1.getFoedselsdato());
+        System.out.println("Alder: " + medlem1.udregnAlder());
+        System.out.println("Er senior: " + medlem1.erSenior());
+        System.out.println("kontigentstatus: " + (medlem1.getKontigentStatus() ? "Aktiv" : "Ikke aktiv"));
+
+        System.out.println(("\nOpdatering af medlem 1..."));
+        medlem1.setFuldeNavn("Sikorsky S-67 Blackhawk");
+        medlem1.setKoen("Non-Binær angrebshelikopter");
+        medlem1.setKontigentStatus(false);
+
+        System.out.println("Opdateret Medlem 1:");
+        System.out.println("Fulde navn: " + medlem1.getFuldeNavn());
+        System.out.println("Køn: " + medlem1.getKoen());
+        System.out.println("Fødselsdato: " + medlem1.getFoedselsdato());
+        System.out.println("Alder: " + medlem1.udregnAlder());
+        System.out.println("Er senior: " + medlem1.erSenior());
+        System.out.println("kontigentstatus: " + (medlem1.getKontigentStatus() ? "Aktiv" : "Ikke aktiv"));
+>>>>>>> Stashed changes
     }
 }
---
+++ src/Medlem.java
@@ -0,0 +1,56 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Medlem {
+
+    private String fuldeNavn;
+    private String koen;
+    private LocalDate foedselsdato;
+    private boolean erSenior;
+    private boolean kontigentStatus;
+
+    public Medlem (String fuldeNavn, String koen, LocalDate foedselsdato, boolean kontigentStatus) {
+        this.fuldeNavn = fuldeNavn;
+        this.koen = koen;
+        this.foedselsdato = foedselsdato;
+        this.kontigentStatus = kontigentStatus;
+    }
+    public String getFuldeNavn(){
+        return fuldeNavn;
+    }
+
+    public void setFuldeNavn(String fuldeNavn){
+        this.fuldeNavn = fuldeNavn;
+    }
+
+    public String getKoen(){
+        return koen;
+    }
+
+    public void setKoen(String koen){
+        this.koen = koen;
+    }
+
+    public LocalDate getFoedselsdato(){
+        return foedselsdato;
+    }
+
+    public boolean getKontigentStatus(){
+        return kontigentStatus;
+    }
+
+    public void setKontigentStatus(boolean kontigentsStatus){
+        this.kontigentStatus = kontigentsStatus;
+    }
+
+    public int udregnAlder() {
+        LocalDate aktuelleDato = LocalDate.now();
+        Period period = Period.between(foedselsdato, aktuelleDato);
+        return period.getYears();
+    }
+
+    public boolean erSenior() {
+        return udregnAlder() > 60;
+    }
+}
+
