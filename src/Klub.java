3aba538383200dd957afc4b7de52f5ed464021b4
djh06c <djdavidjorgen@gmail.com>
2024-11-29 14:00:38 +0100

    Klubbror

--- src/Klub.java
+++ src/Klub.java
@@ -1,17 +1,27 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Klub {
-    ArrayList<KonkurrenceSvoemmer> juniorHold = new ArrayList<>();
-    ArrayList<KonkurrenceSvoemmer> seniorHold = new ArrayList<>();
+    private ArrayList<KonkurrenceSvoemmer> juniorHold = new ArrayList<>();
+    private ArrayList<KonkurrenceSvoemmer> seniorHold = new ArrayList<>();
+
+    public Klub() {
+        juniorHold = new ArrayList<>();
+        seniorHold = new ArrayList<>();
+    }
+
 
     public void tilfoejMedlem(Medlem medlem) {
-
-        if (medlem.erSenior()){
-
-            seniorHold.add((KonkurrenceSvoemmer) medlem);
-        }
-        else {
-            juniorHold.add((KonkurrenceSvoemmer) medlem);
+        if (medlem instanceof KonkurrenceSvoemmer svoemmer) {
+            if (medlem.erSenior()) {
+                seniorHold.add(svoemmer);
+                System.out.println("Medlem er tilføjet til seniorholdet!");
+            } else {
+                juniorHold.add(svoemmer);
+                System.out.println("Medlem er tilføjet til juniorholdet!");
+            }
+        } else {
+            System.out.println("Bro er ikke konkurrence Svømmer. Prøv igen!");
         }
     }
 }
